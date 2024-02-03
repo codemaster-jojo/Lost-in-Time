998ec75f7b829597c44b3ef659cef3c687cdedb9
codemaster-jojo <jonny.masterpi@gmail.com>
2024-02-02 19:40:03 -0800

    hunger bar
    you lose 1 hunger every second, 100 hunger total

--- Assets/HUDManagement.cs
+++ Assets/HUDManagement.cs
@@ -7,12 +7,21 @@ public class HUDManagement : MonoBehaviour
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
 
+    [SerializeField] private int maxHunger = 100;
+    [SerializeField] private int currentHunger;
+
+
     public HealthBar healthBar;
+    public HealthBar hungerBar; // I know its a hunger bar but the name thing is healthbar so...
+
+    float elapsed = 0f;
+
     // Start is called before the first frame update
     private void Awake()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        hungerBar.SetMaxHealth(maxHunger);
     }
 
     // Update is called once per frame
@@ -21,7 +30,18 @@ public class HUDManagement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             TakeDamage(20);
         }
+
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f) {
+            elapsed = elapsed % 1f;
+            GetHungry();
+        }
     }
+    private void GetHungry() {
+        currentHunger -= 1;
+        hungerBar.SetHealth(currentHunger);
+    }
+
     private void TakeDamage(int dmg) {
         currentHealth -= dmg;
         healthBar.SetHealth(currentHealth);
